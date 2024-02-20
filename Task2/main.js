c8b430f32036160e22c54cd82aa0120156b23ed7
nardany <99093283+nardany@users.noreply.github.com>
2024-02-21 01:16:33 +0400

    Add files via upload

--- Task2/main.js
+++ Task2/main.js
@@ -1,1 +1,6 @@
+let text = document.getElementById("text")
+let btn = document.getElementById("btn")
 
+const changeText = function(){
+    text.innerHTML = text.innerHTML === "Text" ? "Changin Text" : "Text"
+}
