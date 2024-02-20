002ab58dd1ae0fa4e8ab5a577ffe9c4bc2e601ae
nardany <99093283+nardany@users.noreply.github.com>
2024-02-21 01:54:53 +0400

    Add files via upload

--- Task3/main.js
+++ Task3/main.js
@@ -1,1 +1,7 @@
-
+let div = document.getElementById("showHideDiv")
+let btn = document.getElementById("btn")
+let condition = false
+const showHideFunction = function(){
+    div.style.display = div.style.display === "block" ? "none" : "block";
+    btn.innerHTML = btn.innerHTML === "Show" ? "Hide" : "Show"
+}
