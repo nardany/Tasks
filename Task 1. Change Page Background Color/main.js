3e6c00d7783074aa8dadde7565c2b76fd9ec3ca0
nardany <99093283+nardany@users.noreply.github.com>
2024-02-21 15:47:00 +0400

    Add files via upload

--- Task 1. Change Page Background Color/main.js
+++ Task 1. Change Page Background Color/main.js
@@ -1,1 +1,13 @@
+let btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
+let btn3 = document.getElementById("btn3")
 
+const changeBColor1 = function(){
+    document.body.style.backgroundColor  = "blue"
+}
+const changeBColor2 = function(){
+    document.body.style.backgroundColor  = "red"
+}
+const changeBColor3 = function(){
+    document.body.style.backgroundColor  = "green"
+}
