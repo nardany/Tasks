74c4748e8328de2ae519d41f66a30d0d7be7e56d
nardany <99093283+nardany@users.noreply.github.com>
2024-02-21 16:27:47 +0400

    Add files via upload

--- Task 3. Show&Hide a Paragraph/main.js
+++ Task 3. Show&Hide a Paragraph/main.js
@@ -1,1 +1,6 @@
-
+let p = document.getElementById("p")
+let btn = document.getElementById("btn")
+const showHideFunction = function(){
+    p.style.display = p.style.display === "block" ? "none" : "block";
+    btn.innerHTML = btn.innerHTML === "Show" ? "Hide" : "Show"
+}
